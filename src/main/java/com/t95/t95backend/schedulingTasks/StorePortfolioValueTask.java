da00afc7f9ad5a2cb560be296aaba57c087c8704
Johnson <poshenc@gmail.com>
2023-03-24 09:56:42 +0800

    [cron schedule] fix: corn scheduled time config

--- src/main/java/com/t95/t95backend/schedulingTasks/StorePortfolioValueTask.java
+++ src/main/java/com/t95/t95backend/schedulingTasks/StorePortfolioValueTask.java
@@ -29,10 +29,9 @@ public class StorePortfolioValueTask {
     }
 
 
-//    @EventListener(ApplicationReadyEvent.class)
-    @Scheduled(cron="01 0 0 * * *")
+//    @EventListener(ApplicationReadyEvent.class) //fire task on app init
+    @Scheduled(cron="0 5 1 * * ?", zone="Asia/Taipei") //fire task at 1:05am TPE time every day
     public void storePortfolioValueAtMidnight() {
-
         //1. get all portfolios (portfolio_id + user_id + cash)
         List<Portfolio> portfolios = portfolioService.getPortfolios();
 
